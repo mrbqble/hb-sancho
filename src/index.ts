6ac9667300a188bab773316eb3958114b9d5c579
Bibol <asetbek.b.1003@gmail.com>
2023-04-13 23:15:22 +0600

    mainCongrats

--- src/index.ts
+++ src/index.ts
@@ -13,14 +13,30 @@ export const congratulations = (person: Person) => {
         console.log(`Happy Birthday, ${person.name}!!!`)
     }
     if (person.isTraumatized) {
-        console.log('Heal faster!!!')
+        setTimeout(() => console.log('Heal faster!!!'), 2000)
     }
     if (person.hasIGCSEIn3Weeks) {
-        console.log('Seriously, get well soon and get that triple A*!')
+        setTimeout(() => console.log('Seriously, get well soon and get that triple A*!'), 5000)
     }
-    console.log('We wish you all the best!!!')
     if (person.isTheBestPerson) {
-        console.log('Always stay kind, empatic, educated and hard-working person as you are!')
+        setTimeout(() => console.log('Always stay kind, empatic, educated and hard-working person as you are!'), 10000)
     }
-    return
+    setTimeout(() => {
+        console.log('We wish you all the best!!!')
+        console.log('Call realCongrats() after you read this')
+    }, 15000)
+    
+}
+
+export const realCongrats = () => {
+    console.log("Let me ask a simple question: are you gay?")
+    setTimeout(() => console.log("Well, we all know that you are gay. It is okay, don't be shy :)"), 5000)
+    setTimeout(() => console.log("So there are some of gay wishes from us. Don't make any more gay decisions, my brother. Also, be careful with your presents, letters and watch your back. You know what we mean :) Check the package functions. There is stil something else waiting for you to call it."), 10000)
+}
+
+export const mainCongrats = () => {
+    console.log('Welcome to the main part of this package! Finally good to see you man.');
+    setTimeout(() => console.log("Ладно шутки в сторону. Дай бог тебе мозгов чтобы ты все таки начал смотреть One Piece (One Piece is REAAAL!!!)."), 5000)
+    setTimeout(() => console.log("12 сыныпта кыз таба алмадын енди, надеемся хотя бы 1-курста табасын."), 10000)
+    setTimeout(() => console.log("Көп сөздің қысқасы, барлығы жақсы болсын. Туылған күнің құтты болсын, біздің сүйікті досымыз!"), 15000)
 }
